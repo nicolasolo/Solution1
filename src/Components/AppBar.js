00bd76b70f53342bb7bf4287d25e95824b1cdf0d
nicolasolo <41766950+nicolasolo@users.noreply.github.com>
2019-03-10 15:57:23 +0200

    a

--- src/Components/AppBar.js
+++ src/Components/AppBar.js
@@ -27,7 +27,7 @@ function ButtonAppBar(props) {
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <AppBar position="fixed">
                 <Toolbar>
                     <Drawer className={classes.menuButton} />
                     <br></br>                       
